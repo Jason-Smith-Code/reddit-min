baaee898d2c22a81c857e43c774dbaf81fe03601
jason-smith-code <jasonsmithcode@gmail.com>
2022-07-01 16:13:38 +0100

    added menu button for offscreen subreddit list

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import logo from "./Logo.png";
 import SearchBar from './features/Search/SearchBar';
 import SubReddits from "./features/SubReddits/SubReddits";
 import Posts from "./features/posts/Posts";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 function App() {
+
+
+  function toggleMenu() {
+
+  }
   return (
     <div className="App">
       <div id="header">
         <img id="logo" alt="Logo" src={logo}></img>
+        <button id="menu" onClick={toggleMenu}><FontAwesomeIcon icon={faBars} /></button>
         <SearchBar />
       </div>
       <div id="main-container">
