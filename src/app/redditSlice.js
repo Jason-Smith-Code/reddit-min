75c5dd11ad1dd6ae5c7e18cbf507a3024519c6ab
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-07-04 20:53:17 +0100

    comment api
    added comment actions and state to reddit slice

--- src/app/redditSlice.js
+++ src/app/redditSlice.js
@@ -2,16 +2,39 @@ import { createSlice } from "@reduxjs/toolkit";
 import {
   getPostsFromReddit,
   getPostsFromRedditWithoutSearch,
+  getCommentsFromPost,
 } from "../api/RedditAPI";
 
-// Thunk
 export const fetchPosts = (prefix, search) => async (dispatch) => {
   try {
     dispatch(loadingPosts());
     const posts = await getPostsFromReddit(prefix, search);
-    dispatch(getPostsSuccess(posts));
+    // Add more attrbutes to the post to hold comment data
+    const postsWithMetadata = posts.map((post) => ({
+      ...post,
+      showingComments: false,
+      comments: [],
+      loadingComments: false,
+      errorComments: false,
+    }));
+    dispatch(getPostsSuccess(postsWithMetadata));
+    // Load comments
   } catch (error) {
     dispatch(getPostFail());
+  }
+};
+
+// /r/Damnthatsinteresting/comments/uy0jxw/over_the_24_hours_since_the_uvalde_massacre_fox/
+export const fetchComments = (index, permalink) => async (dispatch) => {
+  try {
+    dispatch(loadingComments());
+    const comments = await getCommentsFromPost(permalink);
+    console.log(comments);
+    dispatch(getCommentsSuccess({ index, comments }));
+    dispatch(toggleShowingComments(index));
+  } catch (error) {
+    console.log(error);
+    dispatch(getCommentFail());
   }
 };
 
@@ -49,14 +72,42 @@ const redditSlice = createSlice({
     setPostSearchTerm(state, action) {
       state.postSearchTerm = action.payload;
     },
+    loadingComments(state) {
+      console.log("loading comments");
+      state.isLoading = true;
+      state.error = false;
+    },
+    getCommentsSuccess(state, action) {
+      console.log("comments successfully loaded");
+      state.posts[action.payload.index].loadingComments = false;
+      state.posts[action.payload.index].comments = action.payload.comments;
+    },
+    getCommentFail(state, action) {
+      console.log("comments failed");
+      state.posts[action.payload].loadingComments = false;
+      state.posts[action.payload].error = true;
+    },
+    toggleShowingComments(state, action) {
+      state.posts[action.payload].showingComments =
+        !state.posts[action.payload].showingComments;
+    },
   },
 });
 
-export const { loadingPosts, getPostsSuccess, getPostFail, setPostSearchTerm } =
-  redditSlice.actions;
+export const {
+  loadingPosts,
+  getPostsSuccess,
+  getPostFail,
+  setPostSearchTerm,
+  loadingComments,
+  getCommentsSuccess,
+  getCommentFail,
+  toggleShowingComments,
+} = redditSlice.actions;
 
 export default redditSlice.reducer;
 
 // Export a constant which holds the current array of posts
 export const selectPosts = (state) => state.reddit.posts;
 export const selectPostSearchQuery = (state) => state.reddit.postSearchTerm;
+export const selectPostComments = (state) => state.reddit.posts.comments;
