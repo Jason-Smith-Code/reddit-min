dedf71ae6ee3c30c1e2872147cffe001abaf3bc1
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-07-05 22:51:23 +0100

    Update PostList.js
    added some font awesome icons, changed text of posted date

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -6,24 +6,23 @@ import {
   fetchComments,
   selectPostSearchQuery,
   toggleShowingComments,
-  selectPostComments,
 } from "../../app/redditSlice";
 import { selectSelectedSubreddit } from "../../app/subredditSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMessage } from "@fortawesome/free-solid-svg-icons";
+import { faReddit } from "@fortawesome/fontawesome-free-brands";
 
 const PostList = () => {
   const dispatch = useDispatch();
   const prefix = useSelector(selectSelectedSubreddit);
   const posts = useSelector(selectPosts);
   const search = useSelector(selectPostSearchQuery);
-  const viewComments = useSelector(selectPostComments);
+
+  // I need to detect when comments are loading and display some text to the viewer
 
   useEffect(() => {
     dispatch(fetchPosts(prefix, search));
   }, [search, prefix, dispatch]);
-
-  useEffect(() => {
-    console.log("comments have changed");
-  }, [viewComments, dispatch]);
 
   // not all image urls are images, so i need to check before rending them
   function checkIfImage(string) {
@@ -39,73 +38,111 @@ const PostList = () => {
     let timeAgoInHours = timeAgoInMinutes / 60;
     let timeAgoInDays = timeAgoInHours / 24;
     if (timeAgoInHours > 24) {
-      return "posted " + Math.ceil(timeAgoInDays) + " Days ago";
+      return Math.ceil(timeAgoInDays) + " Days ago";
     } else {
-      return "posted " + Math.ceil(timeAgoInHours) + " Hours ago";
+      return Math.ceil(timeAgoInHours) + " Hours ago";
     }
   };
 
   const getComments = (index, permalink) => {
     dispatch(fetchComments(index, permalink));
-    // once comments are successfully loaded toggle display
-    // toggle comments passing in index to toggle the correct post
-    //  dispatch(toggleShowingComments(index));
   };
 
-  const mapComments = (redditPost) => {
-    redditPost.comments.map((comment) => {
-      console.log(comment.body);
-      return <p>{comment.body}</p>;
-    });
+  const toggleCommentDisplay = (index) => {
+    dispatch(toggleShowingComments(index));
   };
 
   return (
     <div>
-      <ul>
-        {posts.map((redditPost, index) => (
-          <li className="post-container" key={redditPost.id}>
-            {/* Subreddit */}
-            <p>{redditPost.subreddit_name_prefixed}</p>
-            {/* Show image if image exists */}
-            {checkIfImage(redditPost.url) ? (
-              <img
-                alt={redditPost.title}
-                className="post-image"
-                src={redditPost.url}
-              ></img>
+      {posts.length < 1 ? (
+        <p className="notification-text">
+          Sorry, it looks like there were no results found for that search. Please try again
+        </p>
+      ) : (
+        ""
+      )}
+      {posts.map((redditPost, index) => (
+        <article className="post-container" key={redditPost.id}>
+          {/* Subreddit */}
+          <p className="margin-bottom bold">
+            <FontAwesomeIcon className="button-icon" icon={faReddit} />
+            {redditPost.subreddit_name_prefixed}
+          </p>
+          {/* Show video > image if image exists else show nothing */}
+          {checkIfImage(redditPost.url) ? (
+            <img
+              alt={redditPost.title}
+              className="post-image margin-bottom"
+              src={redditPost.url}
+            ></img>
+          ) : (
+            ""
+          )}
+          {/* Post title */}
+          <p className="margin-bottom">{redditPost.title}</p>
+          {/* Upvotes */}
+          <p className="margin-bottom">Likes: {redditPost.ups}</p>
+          {/* Author + Hours / Days posted ago */}
+          <p className="margin-bottom">
+            Posted by
+            <span className="bold"> {redditPost.author}</span>{" "}
+            {today(redditPost.created_utc)}
+          </p>
+          {/* Comments - if the comments have been loaded or not*/}
+          {/* display something if the comments are loading */}
+          {redditPost.comments.length > 0 ? (
+            /* Comments - if the comments are showing or not*/
+            redditPost.showingComments ? (
+              <button
+                onClick={() => toggleCommentDisplay(index)}
+                className="comments-button"
+              >
+                Hide comments
+              </button>
             ) : (
-              ""
-            )}
-            {/* Post title */}
-            <p>{redditPost.title}</p>
-            {/* Upvotes */}
-            <p>{redditPost.ups}</p>
-            {/* Comments */}
-            <button onClick={() => getComments(index, redditPost.permalink)}>
-              Display comments
+              <button
+                onClick={() => toggleCommentDisplay(index)}
+                className="comments-button"
+              >
+                Show comments
+              </button>
+            )
+          ) : (
+            <button
+              className="comments-button"
+              onClick={() => getComments(index, redditPost.permalink)}
+            >
+              <FontAwesomeIcon className="button-icon" icon={faMessage} />
+              Load comments
             </button>
-            {/* Use the "Comments" word as a trigger to expand to view all comments */}
-            <p>Number of Comments: {redditPost.num_comments}</p>
-            {/* Author + Hours / Days posted ago */}
-            <p>
-              {redditPost.author} {today(redditPost.created_utc)}
+          )}
+          {/* Display a message to let using know comments are loading */}
+          {redditPost.loadingComments ? (
+            <p className="notification-text">Loading comments...</p>
+          ) : (
+            ""
+          )}
+          {/* Display an error message for comments if they fail to load */}
+          {redditPost.errorComments ? (
+            <p className="notification-text">
+              There has been an error loading the comments
             </p>
-            {redditPost.showingComments
-              ? // Map through all comments
-                redditPost.comments.map((comment) => {
-                  return (
-                    <div className="comment-container" key={comment.id}>
-                      <p>{comment.author}</p>
-                      <p>{comment.body}</p>
-                    </div>
-                  );
-                })
-              : // <p>{redditPost.comments[index].body}</p>
-                "no comments"}
-          </li>
-        ))}
-      </ul>
-      <ul></ul>
+          ) : (
+            ""
+          )}
+          {redditPost.showingComments
+            ? // Map through all comments
+              redditPost.comments.map((comment) => {
+                return (
+                  <blockquote className="comment-container" key={comment.id}>
+                    <p className="margin-bottom bold">{comment.author}</p>
+                    <p>{comment.body}</p>
+                  </blockquote>
+                );
+              })
+            : ""}
+        </article>
+      ))}
     </div>
   );
 };
