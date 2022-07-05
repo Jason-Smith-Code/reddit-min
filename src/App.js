df36d49a93534da2bc491feb48d359cb914adbaf
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-07-05 22:50:12 +0100

    Update App.js
    added menu toggle. replace image logo with html and css text styled logo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import logo from "./Logo.png";
-import SearchBar from './features/Search/SearchBar';
+import SearchBar from "./features/search/SearchBar";
 import SubReddits from "./features/SubReddits/SubReddits";
 import Posts from "./features/posts/Posts";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-
-
+  const [menu, setMenu] = useState(true);
   function toggleMenu() {
-
+    setMenu(!menu);
+    const subredditMenu = document.getElementById("subreddits-list");
+    // if menu is set to true
+    if (menu) {
+      subredditMenu.style.transform = "translateX(0%)";
+    } else {
+      subredditMenu.style.transform = "translateX(100%)";
+    }
   }
   return (
     <div className="App">
       <div id="header">
-        <img id="logo" alt="Logo" src={logo}></img>
-        <button id="menu" onClick={toggleMenu}><FontAwesomeIcon icon={faBars} /></button>
+        <p id="logo-text">
+          <span id="reddit">Reddit</span>
+          <span id="mini">Mini</span>
+        </p>
+        <button id="menu" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
         <SearchBar />
       </div>
       <div id="main-container">
         <Posts />
-        <SubReddits />
+        <SubReddits menu={menu} setMenu={setMenu} />
       </div>
     </div>
   );
