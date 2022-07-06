cbd2c08984117d46a9123cc2be6ccf9a8ebb5d13
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-07-06 21:31:27 +0100

    Create BackToTop.js
    added back to top button component

---
+++ src/features/backToTop/BackToTop.js
@@ -0,0 +1,19 @@
+import "./BackToTop.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
+
+const BackToTop = () => {
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
+  return (
+    <button onClick={topFunction} id="back-top-top">
+      <FontAwesomeIcon size="xl" icon={faAngleUp} />
+    </button>
+  );
+};
+
+export default BackToTop;
