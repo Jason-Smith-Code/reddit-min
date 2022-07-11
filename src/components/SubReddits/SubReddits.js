9e27672d2411ae3462f60a839b175885700d6110
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-07-11 11:47:29 +0100

    Update SubReddits.js
    added a data test id for the subreddit list container

--- src/components/SubReddits/SubReddits.js
+++ src/components/SubReddits/SubReddits.js
@@ -4,7 +4,7 @@ import React from "react";
 const SubReddits = ({ menu, setMenu }) => {
   // const currentArray = useSelector((state) => state.reddit.redditPosts);
   return (
-    <div id="subreddits-list">
+    <div data-testid="subreddits-list" id="subreddits-list">
       <h2>Subreddits</h2>
       <SubredditList menu={menu} setMenu={setMenu} />
     </div>
