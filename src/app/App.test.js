10d8b45e5d85d53b11f37ef2b667b984a314423e
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-07-11 11:44:28 +0100

    Update App.test.js
    removed tests for logo as the logo has its own test file now

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,36 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-import { Provider } from "react-redux";
-import store from "../redux/store";
+import "@testing-library/jest-dom/extend-expect";
 
-describe("<App />", () => {
-
-  beforeEach(() => {
-    render(
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
-  });
-
-  test("The logo renders", () => {
-
-    const logo = screen.getByRole("logo");
-    expect(logo).toBeInTheDocument();
-  });
-
-  test("The menu can be toggled in and out of view (on mobile screen) ", () => {
-    // Set window width to less that 1000 px
-    window.innerWidth = "1200"
-    expect(window.innerWidth).toBe("1200")
-
-    const menu = screen.getByText("Menu")
-    expect(menu).toBeTruthy()
-    // click the menu
-    // check that the subreddit content is displaying on the screen
-
-    // click the menu
-
-    // check that the subreddit content is not displaying on the screen
-  });
+describe("Name of the group", () => {
+  test("should ", () => {});
 });
