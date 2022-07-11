ca52e4884adbe4ff23b07a7a8081ea2bdca72d97
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-07-11 11:42:35 +0100

    Update BackToTop.test.js
    test complete

--- src/components/backToTop/BackToTop.test.js
+++ src/components/backToTop/BackToTop.test.js
@@ -2,7 +2,7 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import App from "../../app/App";
 import { Provider } from "react-redux";
 import store from "../../redux/store";
-import { toHaveValue } from "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect"; // Adds extra assertations for Expect. Adds better content specific error messages
 
 describe("<BackToTop />", () => {
   beforeEach(() => {
@@ -18,14 +18,14 @@ describe("<BackToTop />", () => {
   });
 
   test("When clicked, user scrolls to top of screen", () => {
-    // scroll to bottom of page
-    fireEvent.scroll(window, { scrollY: "1000" });
-    // verify vertical position
-    let windowScrollYPosition = window.scrollY;
-
+    // Spy on window ScrollTo
+    global.scrollTo = jest.fn();
+    const scrolling = jest.spyOn(window, "scrollTo");
+    // Identify the scroll button
     const button = screen.getByTestId("back-to-top");
+    // Click the scroll button
     fireEvent.click(button);
-
-    // verify vertical position
+    // Check if the window scroll function has been called
+    expect(scrolling).toHaveBeenCalled();
   });
 });
