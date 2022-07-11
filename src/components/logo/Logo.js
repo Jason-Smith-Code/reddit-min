28f5b5808dfa7a736f0d95f620c971df9a3897c2
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-07-11 11:43:11 +0100

    Create Logo.js
    created a new component to hold the logo styling

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,10 @@
+import './Logo.css'
+
+export const Logo = () => {
+  return (
+    <h1 id="logo-text">
+      <span id="reddit">Reddit</span>
+      <span id="mini">Mini</span>
+    </h1>
+  );
+};
