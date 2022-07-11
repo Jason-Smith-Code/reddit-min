f8c95b0f027f135a2686df808cba30177c8b323d
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-07-11 11:42:26 +0100

    Update BackToTop.js
    changed the method of scrolling to "scrollTo" as I could test this/

--- src/components/backToTop/BackToTop.js
+++ src/components/backToTop/BackToTop.js
@@ -5,8 +5,11 @@ import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
 const BackToTop = () => {
   // When the user clicks on the button, scroll to the top of the document
   function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
   }
 
   return (
